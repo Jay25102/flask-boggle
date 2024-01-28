94794f320702bacd60fd9da5bb6c7831aafb3d11
jay25102 <jasonhuang25102@gmail.com>
2024-01-28 10:24:27 -0500

    testfile

--- test.py
+++ test.py
@@ -7,4 +7,17 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        self.client = app.test_client()
+        app.config["TESTING"] = True
+
+    def test_gameboard(self):
+        with self.client:
+            response = self.client.get("/board")
+            self.assertIn(session.get("highscore"))
+            self.assertIn(session.get("nplays"))
+            self.assertIn(b'<h2>Score', response.data)
+            self.assertIn(b'<div>Played:', response.data)
+            self.assertIn(b'<div>Highscore:', response.data)
+            self.assertIn(b'<div>Time left:', response.data)
 
